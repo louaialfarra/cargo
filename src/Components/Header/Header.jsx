8b067dd28566ca832b2af90372d5de3a5f9fb3c4
louaialfarra <156582409+louaialfarra@users.noreply.github.com>
2025-07-01 15:57:05 +0300

    add hero need to fix the positions
    check the video

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className="headerContainer">
+        <div className="logo">Logo Here</div>
+        <div className="menuList">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+
+            <li>Contact US</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Header;
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Header/Header";
+import "./Hero.css";
+const Hero = () => {
+  return (
+    <div className="heroContainer">
+      <Header />
+      <p>hello Hero</p>
+    </div>
+  );
+};
+
+export default Hero;
