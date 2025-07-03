cd45b82978ee1dfbb0d49b46a2399220e66efa2b
louaialfarra <156582409+louaialfarra@users.noreply.github.com>
2025-07-03 15:41:40 +0300

    add some fixtes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,13 +4,20 @@ const Header = () => {
   return (
     <>
       <div className="headerContainer">
-        <div className="logo">Logo Here</div>
+        <div className="logo">
+          <a href="#">Logo Here</a>
+        </div>
         <div className="menuList">
           <ul>
-            <li>Home</li>
-            <li>About</li>
-
-            <li>Contact US</li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact Us</a>
+            </li>
           </ul>
         </div>
       </div>
