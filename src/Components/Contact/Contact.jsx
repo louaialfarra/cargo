e582c5698f8e0cc16d199c7489f8584e9e57a660
louaialfarra <156582409+louaialfarra@users.noreply.github.com>
2025-07-10 15:06:05 +0300

    add some code

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -5,6 +5,7 @@ const Contact = () => {
   const [formData, setFormData] = useState({
     from_name: "",
     reply_to: "",
+    massage: "",
   });
 
   const handleChange = (e) => {
@@ -15,21 +16,33 @@ const Contact = () => {
   return (
     <>
       <div className="contactContainer">
-        <h1>contact US</h1>
+        <div className="contactTitle">
+          {" "}
+          <h1>Contact US</h1>
+        </div>
         <form>
+          <div className="emailContainer">
+            <textarea
+              placeholder="Ask any Quesiotn"
+              name="massage"
+              onChange={handleChange}
+            />
+          </div>
+          name:
           <input
             type="text"
             placeholder="name"
             onChange={handleChange}
             name="from_name"
           />
+          email:
           <input
             type="text"
             name="reply_to"
             placeholder="Enter ur Email"
             onChange={handleChange}
           />
-          <textarea placeholder="Ask any Quesiotn" />
+          <button type="submite">Submite</button>
         </form>
       </div>
     </>
